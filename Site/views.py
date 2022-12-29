9a671da32ad46467c450fe9d305642d62b9b71c7
ehab011 <abdelrhman.wahdan@ejust.edu.eg>
2022-12-29 02:42:40 +0200

    Adding products to cart

--- Site/views.py
+++ Site/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
-from Site.models import *
+from .models import *
+
 def Store(request):
-    products = Product.objects.all()
-    context = {'products': products}
-    return render(request,'Site/Store.html', context)
+	products = Product.objects.all()
+	context = {'products':products}
+	return render(request, 'Site/Store.html', context)
+
 def Cart(request):
-    context = {}
-    return render(request,'Site/Cart.html', context)
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		items = order.orderitem_set.all()
+	else:
+		#Create empty cart for now for non-logged in user
+		items = []
+		order = {'get_cart_total':0, 'get_cart_items':0}
+
+	context = {'items':items, 'order':order}
+	return render(request, 'Site/Cart.html', context)
+
 def Checkout(request):
-    context = {}
-    return render(request, 'Site/Checkout.html', context)
+	context = {}
+	return render(request, 'Site/checkout.html', context)
