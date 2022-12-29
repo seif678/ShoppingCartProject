c915c492da357c11f3f56cf5599bd4209cbbc53d
SeifAldin Khaled <seifmohamed678@gmail.com>
2022-12-29 17:44:37 +0200

    Adding Some functionalities to cart and checkout html files

--- Site/models.py
+++ Site/models.py
@@ -43,12 +43,14 @@ class Order(models.Model):
 	def get_cart_total(self):
 		orderitems = self.orderitem_set.all()
 		total = sum([item.get_total for item in orderitems])
+
 		return total
 
 	@property
 	def get_cart_items(self):
 		orderitems = self.orderitem_set.all()
 		total = sum([item.quantity for item in orderitems])
+
 		return total
 
 class OrderItem(models.Model):
