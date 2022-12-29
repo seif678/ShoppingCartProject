e4ad7372279ae7d75f21879257e64008e64ceb5e
SeifAldin Khaled <seifmohamed678@gmail.com>
2022-12-29 23:34:36 +0200

    Adding functionalities to buttons in store, cart and checkout pages

--- Site/urls.py
+++ Site/urls.py
@@ -4,7 +4,9 @@ from . import views
 urlpatterns = [
     path('',views.Store,name="Store"),
     path('cart/', views.Cart, name="Cart"),
-    path('checkout/', views.Checkout, name="Checkout")
+    path('checkout/', views.Checkout, name="Checkout"),
+    path('update_item/', views.updateItem, name="udate_item"),
+    path('process_order/',views.processOrder,name="process_order")
 
 ]
 
